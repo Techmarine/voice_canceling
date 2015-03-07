0de204a3ef9406c0f5569d7a79dfd66b5d179ec6
UserN1726 <techmarine712@gmail.com>
2015-03-07 14:25:56 +0300

    All files converted to mp3 format. ffmpeg converter added.

---
+++ wav2mp3.bat
@@ -0,0 +1,4 @@
+ffmpeg -i Caros_filtered.wav Caros_filtered.mp3
+ffmpeg -i Dorma_filtered.wav Dorma_filtered.mp3
+ffmpeg -i Moelleux_filtered.wav Moelleux_filtered.mp3
+ffmpeg -i Ostina_filtered.wav Ostina_filtered.mp3
